46a8fb35f45a8f10643cffd003b4c4f6cc829ff5
Mark Njoroge <marknjo7@gmail.com>
2021-02-20 15:09:30 +0300

    add error handler to the notes page

--- src/app/notes/error.tsx
+++ src/app/notes/error.tsx
@@ -5,7 +5,7 @@ import Link from 'next/link';
 export default function Error() {
   return (
     <p>
-      ⛔ Error loading note. Take me to safety <Link href='/notes'>Now 🚶‍♀️</Link>
+      ⛔ Error loading notes. Take me to safety <Link href='/'>Now 🚶‍♀️</Link>
     </p>
   );
 }
