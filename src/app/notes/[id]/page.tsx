85fd66e39c531a6627c53564dd330d5cbc84df28
Mark Njoroge <marknjo7@gmail.com>
2021-02-20 14:56:05 +0300

    add single page data fetching getNote method

--- src/app/notes/[id]/page.tsx
+++ src/app/notes/[id]/page.tsx
@@ -1,4 +1,21 @@
-export default async function NotePage() {
+import { INote } from '@/src/types/INote';
+
+async function getNote(noteId: string) {
+  const res = await fetch(
+    `http://127.0.0.1:8090/api/collection/notes/record/${noteId}`,
+    {
+      next: { revalidate: 10 },
+    }
+  );
+
+  const note = await res.json();
+
+  return note as INote;
+}
+
+export default async function NotePage({ params }: { params: { id: string } }) {
+  const note = await getNote(params.id);
+
   return (
     <div>
       <h1>Notes</h1>
