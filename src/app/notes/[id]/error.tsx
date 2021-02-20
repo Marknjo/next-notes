1994b24e1e34782e76d0adae955a9d00746376a5
Mark Njoroge <marknjo7@gmail.com>
2021-02-20 15:06:41 +0300

    add error handler to the notes single page

---
+++ src/app/notes/[id]/error.tsx
@@ -0,0 +1,9 @@
+import Link from 'next/link';
+
+export default function Error() {
+  return (
+    <p>
+      ⛔ Error loading note. Take me to safety <Link href='/'>Now 🚶‍♀️</Link>
+    </p>
+  );
+}
