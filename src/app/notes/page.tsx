108d464b28e78b11cb82b40542a0cecad5611b58
Mark Njoroge <marknjo7@gmail.com>
2021-02-27 18:18:27 +0300

    update note component to inclede delete method

--- src/app/notes/page.tsx
+++ src/app/notes/page.tsx
@@ -5,6 +5,7 @@ import styles from './Notes.module.css';
 import AddNote from './AddNote';
 import { addNote } from './actions';
 import { revalidatePath } from 'next/cache';
+import DeleteNote from './DeleteNote';
 
 async function getNotes() {
   const res = await fetch(
@@ -74,7 +75,10 @@ function Note({ content, title, id, created }: INote) {
       <article className={styles.note}>
         <h2 className={styles.title}>{title}</h2>
         <h5 className={styles.content}>{content}</h5>
-        <p className={styles.created}>{created}</p>
+        <footer className={styles.footer}>
+          <p className={styles.created}>{created}</p>
+          <DeleteNote id={id} />
+        </footer>
       </article>
     </Link>
   );
